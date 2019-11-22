430f98fa15f1b795d1ab0e9bc455bd0aaee8491c
Omari Blockton <oblockton@gmail.com>
2019-11-22 05:35:50 -0800

    update readme

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,4 @@
-// function buildMetadata(sample) {
-//
-//
-//   // @TODO: Complete the following function that builds the metadata panel
-//
-//   // Use `d3.json` to fetch the metadata for a sample
-//     // Use d3 to select the panel with id of `#sample-metadata`
-//
-//     // Use `.html("") to clear any existing metadata
-//
-//     // Use `Object.entries` to add each key and value pair to the panel
-//     // Hint: Inside the loop, you will need to use d3 to append new
-//     // tags for each key-value in the metadata.
-//
-//     // BONUS: Build the Gauge Chart
-//     // buildGauge(data.WFREQ);
-// }
+
 
 function buildMetadata(sample) {
   let url = `/metadata/${sample}`;
@@ -55,7 +39,7 @@ function buildCharts(sample) {
 
     var data_trace = [trace1];
     var layout = {
-      title: "Belly Button size(to scale)",
+      title: "Sample size",
       xaxis:{title:'ID'},
       showlegeng:true
     };
